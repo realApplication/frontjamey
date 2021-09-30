328f218968bbbe6f41c2600df4b39f2423897b3f
samah-abujwaied <samah.abujweed@yahoo.com>
2021-09-30 12:20:33 +0300

    createReact

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './componants/header';
+import Main from './componants/main'
+import Footer from './componants/footer'
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Main />
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
